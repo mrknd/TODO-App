679b3c27494830047cbd90712865568c25cc9f29
mrknd <markiian.design@gmail.com>
2024-02-16 13:13:55 +0100

    add task func

---
+++ todo_app/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Task
+
+
+# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task,)
+    return redirect('home_page')
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from todo_app.models import Task
+
 
 def home(request):
-    return render(request=request, template_name='home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request=request, template_name='home.html', context=context)
